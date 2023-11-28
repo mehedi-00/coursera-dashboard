dabdc58df61e2d77dead5f06f4bb3805436454f6
mehedidev <mdmehedi23234@gmail.com>
2023-11-29 00:40:05 +0600

    starter file

---
+++ src/components/core/Shared/RightSidebar/RightSidebar.jsx
@@ -0,0 +1,10 @@
+
+const RightSidebar = () => {
+    return (
+        <div className="fixed right-0 top-0 " style={{zIndex: 99999}}>
+           rightSidebar 
+        </div>
+    );
+};
+
+export default RightSidebar;
