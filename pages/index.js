ace0f84491dfc60f1118e5dca1d41c2ff34da786
mehedidev <mdmehedi23234@gmail.com>
2023-11-29 20:45:44 +0600

    dark them set done

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-   <>
-   <div className='text-2xl '>
-    Hello World
-   </div>
-   </>
-  )
+    <>
+      <div className="text-2xl ">Hello World</div>
+    </>
+  );
 }
